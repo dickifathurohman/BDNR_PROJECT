7c9ad6f6917d067e3c13e7e2eb51da6716101190
fachri komputer <fachrikomputer@haha.com>
2025-01-04 02:15:07 +0700

    baru add dan delete bansos

--- helper_functions/crud.py
+++ helper_functions/crud.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 from connect import *
 
+
 def add_kemiskinan(tahun, provinsi, kota, ppm, ikk, ipk, gk, ppk):
-    
+
     data_id = f"{provinsi}_{tahun}"
 
     # Cek apakah data untuk provinsi, kota, dan tahun sudah ada
@@ -62,7 +63,58 @@ def add_kemiskinan(tahun, provinsi, kota, ppm, ikk, ipk, gk, ppk):
 
         st.success("Data berhasil ditambahkan!")
 
+
+def add_bansos(tahun, provinsi, ppm, ikk, ipk, gk):
+
+    data_id = f"{provinsi}_{tahun}"
+
+    # Cek apakah data bansos sudah ada
+    existing_data = collection.find_one({
+        "_id": data_id,
+        "bansos.rencana_keluarga_penerima": {"$exists": True}
+    })
+
+    if existing_data:
+        st.error(f"Data untuk {provinsi} pada tahun {tahun} sudah ada!")
+    else:
+        # Cek apakah data provinsi dan tahun sudah ada
+        provinsi_tahun_data = collection.find_one({
+            "_id": data_id
+        })
+
+        if provinsi_tahun_data:
+            # Tambahkan kota baru ke data provinsi dan tahun tersebut
+            collection.update_one(
+                {"_id": provinsi_tahun_data["_id"]},
+                {
+                    "$set": {
+                        "bansos.rencana_keluarga_penerima": ppm,
+                        "bansos.realisasi_keluarga_penerima": ikk,
+                        "bansos.rencana_anggaran": ipk,
+                        "bansos.realisasi_anggaran": gk
+                    }
+                }
+            )
+        else:
+            # Jika data provinsi dan tahun belum ada, tambahkan data baru
+            new_data = {
+                "_id": data_id,
+                "nama_provinsi": provinsi,
+                "tahun": tahun,
+                "bansos": {
+                    "rencana_keluarga_penerima": ppm,
+                    'realisasi_keluarga_penerima': ikk,
+                    'rencana_anggaran': ipk,
+                    'realisasi_anggaran': gk
+                }
+            }
+            collection.insert_one(new_data)
+
+        st.success("Data berhasil ditambahkan!")
+
 # Fungsi untuk menghapus data kota
+
+
 def delete_kemiskinan(data_id, kota):
     # Cek apakah data untuk provinsi, kota, dan tahun ada
     existing_data = collection.find_one({
@@ -92,7 +144,42 @@ def delete_kemiskinan(data_id, kota):
             "_id": data_id,
         })
 
-        if not updated_data["data_kota"] and not updated_data.get("bantuan_sosial") and not updated_data.get("pengangguran"):
+        if not updated_data["data_kota"] and not updated_data.get("bansos") and not updated_data.get("pengangguran"):
+            collection.delete_one({
+                "_id": data_id
+            })
+
+        st.success(f"Data berhasil dihapus!")
+
+
+def delete_bansos(data_id, provinsi):
+    # Cek apakah data untuk provinsi, kota, dan tahun ada
+    existing_data = collection.find_one({
+        "_id": data_id,
+        "nama_provinsi": provinsi
+    })
+
+    if not existing_data:
+        st.error(f"Data tidak ditemukan!")
+    else:
+        # Hapus data kota dari provinsi dan tahun tersebut
+        collection.update_one(
+            {
+                "_id": data_id
+            },
+            {
+                "$set": {
+                    "bansos": {}  # Mengubah 'bansos' menjadi objek kosong
+                }
+            }
+        )
+
+        # Setelah penghapusan, cek apakah data provinsi masih memiliki data kota
+        updated_data = collection.find_one({
+            "_id": data_id,
+        })
+
+        if not updated_data["data_kota"] and not updated_data.get("bansos") and not updated_data.get("pengangguran"):
             collection.delete_one({
                 "_id": data_id
             })
@@ -100,6 +187,8 @@ def delete_kemiskinan(data_id, kota):
         st.success(f"Data berhasil dihapus!")
 
 # Fungsi untuk mengupdate data kemiskinan berdasarkan _id dan nama kota
+
+
 def update_kemiskinan(_id, nama_kota, updated_values):
 
     # Query untuk mencocokkan dokumen dengan _id dan nama_kota
@@ -121,4 +210,3 @@ def update_kemiskinan(_id, nama_kota, updated_values):
         st.success("Data berhasil diperbarui!")
     else:
         st.error("Gagal memperbarui data. Periksa kembali ID dan Nama Kota.")
-    
--- helper_functions/dataHandler.py
+++ helper_functions/dataHandler.py
@@ -3,39 +3,46 @@ import pandas as pd
 from connect import *
 
 # Fungsi untuk memuat data dari MongoDB
+
+
 def load_data():
     cursor = collection.find().sort([("nama_provinsi", 1), ("tahun", 1)])
     data = list(cursor)
     return data
 
 # Sidebar filter untuk provinsi, kota, dan tahun
+
+
 def sidebar_filters(data):
     st.sidebar.title("Filter")
 
     # Filter provinsi
     provinsi_selected = st.sidebar.selectbox(
-        "Pilih Provinsi", 
+        "Pilih Provinsi",
         options=["Semua"] + list(set(item['nama_provinsi'] for item in data))
     )
 
     # Filter kota
     if provinsi_selected == "Semua":
         kota_options = ["Semua"] + list(
-            set(kota['nama_kota'] for item in data for kota in item['data_kota'])
+            set(kota['nama_kota']
+                for item in data for kota in item['data_kota'])
         )
     else:
         kota_options = ["Semua"] + list(
-            set(kota['nama_kota'] for item in data if item['nama_provinsi'] == provinsi_selected for kota in item['data_kota'])
+            set(kota['nama_kota'] for item in data if item['nama_provinsi']
+                == provinsi_selected for kota in item['data_kota'])
         )
-    
+
     kota_selected = st.sidebar.selectbox("Pilih Kota", options=kota_options)
 
     # Filter tahun
     tahun_min, tahun_max = st.sidebar.slider(
-        "Filter Rentang Tahun", 
-        min_value=int(min(item['tahun'] for item in data)), 
+        "Filter Rentang Tahun",
+        min_value=int(min(item['tahun'] for item in data)),
         max_value=int(max(item['tahun'] for item in data)),
-        value=(int(min(item['tahun'] for item in data)), int(max(item['tahun'] for item in data)))
+        value=(int(min(item['tahun'] for item in data)),
+               int(max(item['tahun'] for item in data)))
     )
 
     # Filter data berdasarkan provinsi, kota, dan tahun
@@ -45,18 +52,28 @@ def sidebar_filters(data):
 
     return filtered_data, provinsi_selected, kota_selected
 
-def flatten_data(data):
+
+def flatten_data(data, flatten_style='kemiskinan'):
     """
     Jabarkan data kota dari setiap provinsi dan tahun ke dalam daftar datar.
     """
     flattened_data = []
-    for item in data:
-        for kota in item['data_kota']:
+    if flatten_style == 'kemiskinan':
+        for item in data:
+            for kota in item['data_kota']:
+                flattened_data.append({
+                    "_id": item["_id"],
+                    "tahun": item["tahun"],
+                    "nama_provinsi": item["nama_provinsi"],
+                    "nama_kota": kota["nama_kota"],
+                    "kemiskinan": kota["kemiskinan"],
+                })
+    elif flatten_style == 'bansos':
+        for item in data:
             flattened_data.append({
-                "_id"  : item["_id"],
+                "_id": item["_id"],
                 "tahun": item["tahun"],
                 "nama_provinsi": item["nama_provinsi"],
-                "nama_kota": kota["nama_kota"],
-                "kemiskinan": kota["kemiskinan"]
+                "bansos": item["bansos"],
             })
     return flattened_data
