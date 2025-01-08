0d6c1ea561f8b5b6ca32d883270969db90eee2f9
Dicki Fathurohman <dickifathurohman06@gmail.com>
2025-01-08 20:47:40 +0700

    connect server

--- connect.py
+++ connect.py
@@ -1,7 +1,11 @@
 import pymongo
 
+# connect ke server
+uri = "mongodb+srv://fachrinajmnoer:FshsIDEkArEfJZsY@utscluster.4lijn.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri)
+
 # connect ke lokal
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+#client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 db = client['db_kemiskinan']
 
